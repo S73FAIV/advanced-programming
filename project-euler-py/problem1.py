b604d3e7bb8903c84fd46131a84d80631aad6ef4
stefan.willibald@jambit.com <stefan.willibald@jambit.com>
2025-09-29 12:22:56 +0200

    add first project euler solutions

---
+++ project-euler-py/problem1.py
@@ -0,0 +1,28 @@
+####################################################
+# Multiples of 3 or 5
+#
+# Problem 1
+#
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+######################################################
+
+
+# 1st Step: find all multiples of 3 or 5 below 1000
+
+multiples = []
+
+for number in range(1, 1000):
+    if (number % 3) == 0 or (number % 5) == 0:
+        multiples.append(number)
+
+# 2nd step: sum up all items
+
+sum = 0
+for number in multiples:
+    sum += number
+
+print(sum)
---
+++ project-euler-py/problem3.py
@@ -0,0 +1,11 @@
+####################################################
+# Largest Prime Factor
+#
+# Problem 3
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143?
+######################################################
+
+# step 1: google prime factorization algorithms
